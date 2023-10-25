9deeb051fd925cb10c3f84e1338717561608fde7
Casper Holten <casho@itu.dk>
2023-10-25 14:11:02 +0200

    Handin 3

--- MandatoryAssignments/ChittyChat/Client.go
+++ MandatoryAssignments/ChittyChat/Client.go
@@ -1,5 +1,105 @@
-package ChittyChat
+package main
 
-func Publish(Message string) {
+import (
+	"bufio"
+	"chittychat/chittychat"
+	"context"
+	"errors"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
 
+var lamportTime int32 = 1
+var username string
+
+func main() {
+	fmt.Print("Please input a username: ")
+	fmt.Scanln(&username)
+	fmt.Printf("Username is: %s\n", username)
+
+	// Create a logger, Courtesy of https://stackoverflow.com/questions/19965795/how-to-write-log-to-file
+	f, err := os.OpenFile("LogFile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := chittychat.NewChittyChatClient(conn)
+	stream, err := client.Messages(context.Background())
+	if err != nil {
+		log.Fatalf("error while creating stream: %v", err)
+	}
+
+	sendMessage(fmt.Sprintf("Client %s joined Chitty-Chat", username), stream)
+
+	go receiveMessage(stream)
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text, _ := reader.ReadString('\n')
+		sendMessage(strings.TrimSpace(text), stream)
+	}
+}
+
+func incrementLamport() {
+	lamportTime++
+}
+
+func sendMessage(message string, stream chittychat.ChittyChat_MessagesClient) {
+	err := validateMessage(message)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	incrementLamport()
+	msg := &chittychat.Message{
+		Username:  username,
+		Message:   message,
+		Timestamp: lamportTime,
+	}
+
+	log.Printf("Client %s, Outgoing  - %s", username, msg)
+
+	if err := stream.Send(msg); err != nil {
+		fmt.Printf("Failed to send message: %v", err)
+	}
+}
+
+func receiveMessage(stream chittychat.ChittyChat_MessagesClient) {
+	for {
+		incoming, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("Failed to receive a message: %v", err)
+			return
+		}
+		lamportTime = max(lamportTime, incoming.Timestamp) + 1
+		log.Printf("Client %s, Incoming  - %s: %s at Lamport time %d", username, incoming.Username, incoming.Message, incoming.Timestamp)
+	}
+}
+
+func validateMessage(msg string) (err error) {
+
+	if len(msg) > 128 {
+		err = errors.New("message is too long. Keep it within 128 characters long")
+		return
+	}
+
+	if !utf8.ValidString(msg) {
+		err = errors.New("message is too long. Keep it within 128 characters long")
+		return
+	}
+
+	return
 }
--- MandatoryAssignments/ChittyChat/Server.go
+++ MandatoryAssignments/ChittyChat/Server.go
@@ -1,21 +1,102 @@
-package ChittyChat
+package main
 
 import (
-	"fmt",
-	"log",
-	"ChittyChat.proto"
+	"chittychat/chittychat"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
 )
 
 type server struct {
-	//clients []chittychat.
+	chittychat.UnimplementedChittyChatServer
+	clients      []chittychat.ChittyChat_MessagesServer
+	usernames    map[chittychat.ChittyChat_MessagesServer]string
+	lamportClock int32
 }
 
-func Main() {
-	for {
+func (s *server) updateAndIncrementLamport(receivedTimestamp int32) int32 {
+	s.lamportClock = max(s.lamportClock, receivedTimestamp) + 1
+	return s.lamportClock
+}
 
+func max(a, b int32) int32 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func (s *server) Messages(stream chittychat.ChittyChat_MessagesServer) error {
+	s.clients = append(s.clients, stream)
+
+	// infinite loop
+	for {
+		incoming, err := stream.Recv()
+		if err != nil {
+			s.removeClientAndNotify(stream)
+			return err
+		}
+
+		incoming.Timestamp = s.updateAndIncrementLamport(incoming.Timestamp)
+		s.usernames[stream] = incoming.Username
+
+		s.broadcastMessage(incoming)
 	}
 }
 
-func Broadcast() {
+func (s *server) removeClientAndNotify(targetStream chittychat.ChittyChat_MessagesServer) {
 
+	s.updateAndIncrementLamport(s.lamportClock)
+
+	for i, client := range s.clients {
+		if client == targetStream {
+			s.clients = append(s.clients[:i], s.clients[i+1:]...)
+
+			username := s.usernames[client]
+			disconnectMsg := &chittychat.Message{
+				Username:  "Server",
+				Message:   fmt.Sprintf("%s left Chitty-Chat", username),
+				Timestamp: s.lamportClock,
+			}
+
+			log.Printf("Server  -  %s", disconnectMsg)
+
+			s.broadcastMessage(disconnectMsg)
+			break
+		}
+	}
+}
+
+func (s *server) broadcastMessage(msg *chittychat.Message) {
+
+	log.Printf("Server, Outgoing  -  %s", msg)
+
+	for _, client := range s.clients {
+		if err := client.Send(msg); err != nil {
+			log.Printf("Failed to broadcast message: %v", err)
+		}
+	}
+}
+
+func main() {
+
+	// Create a logger, Courtesy of https://stackoverflow.com/questions/19965795/how-to-write-log-to-file
+	f, err := os.OpenFile("LogFile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	lis, _ := net.Listen("tcp", ":50051")
+	grpcServer := grpc.NewServer()
+
+	service := &server{
+		usernames: make(map[chittychat.ChittyChat_MessagesServer]string),
+	}
+
+	chittychat.RegisterChittyChatServer(grpcServer, service)
+	grpcServer.Serve(lis)
 }
